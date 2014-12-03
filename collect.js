15fce3acb44c8e353af8bc92a8c67f22b74443c4
momo9 <liangchenchi@gmail.com>
2014-12-03 15:27:17 +0800

    ex8

---
+++ collect.js
@@ -0,0 +1,14 @@
+var bl = require('bl')
+var http = require('http')
+
+var url = process.argv[2]
+http.get(url, function (resp) {
+	resp.pipe(bl(function (err, data) {
+		if (err) {
+			return console.error(err)
+		}
+		var str = data.toString()
+		console.log(str.length)
+		console.log(str)
+	}))
+})
